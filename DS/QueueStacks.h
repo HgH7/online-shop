3a6e3a160a1ff6fcadfa13c8dfbce1290038a998
Hassan ElSayed <ahsn17462@gmail.com>
2025-12-11 18:36:27 +0200

    finishing the foundation

--- DS/QueueStacks.h
+++ DS/QueueStacks.h
@@ -1,8 +1,27 @@
-//
-// Created by Hassan Elsayed on 26/11/2025.
-//
-
 #ifndef ONLINE_SHOP_QUEUESTACKS_H
 #define ONLINE_SHOP_QUEUESTACKS_H
+
+class ReturnStack{
+public:
+    int top;
+    int maxSize;
+    string* items;
+
+
+
+    ReturnStack(int size){
+
+        maxSize = size;
+        items = new string[maxSize];
+
+        top = -1;
+
+    }
+
+    void pushReturn(string item);
+    void processReturn();
+
+};
+
 
 #endif //ONLINE_SHOP_QUEUESTACKS_H
